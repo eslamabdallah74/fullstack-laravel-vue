2badfae5063601402a14ee7814590a3e033f4ca4
eslamabdallah74 <eslamabdallah301@gmail.com>
2021-08-01 22:29:23 +0200

    Update login.vue
    Better Look

--- resources/js/admin/pages/login.vue
+++ resources/js/admin/pages/login.vue
@@ -2,79 +2,68 @@
     <div>
         <div class="container">
             <div class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20 col-md-4">
-                  <div class="login_header">
-                      <h1>Login to the dashboard</h1>
-                  </div>
-                  <div class="space">
-                        <Input type="email" v-model="data.email"  placeholder="Email"  />
-                    </div>
-                    <div class="space">
-                        <Input type="password" v-model="data.password"  placeholder="******"  />
-                    </div>
-                    <div class="login_footer">
-                        <Button type="primary" @click="login" :disabled="isLogging" :loading="isLogging">{{isLogging ? 'Loging...' : 'Login'}}</Button>
-                    </div>
+                <div class="login-header">
+                    <h2>Login To Dashboard</h2>
+                </div>
+                <div>
+                    <Input type="email" v-model="data.email" placeholder="Enter Email"  />
+                </div>
+                    <br>
+                <div>
+                    <Input type="password" v-model="data.password" placeholder="Enter Password"  />
+                </div>
+                <div class="login-footer">
+                    <Button type="primary" @click="login" >{{isLogging ? 'Login' : 'Login'}}</Button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-
 <script>
 export default {
-    data(){
+    data() {
         return {
-            data : {
-                email : '', 
+            data: {
+                email: '',
                 password: ''
-            }, 
-            isLogging: false, 
+            },
+            isLogging: false,
         }
-    }, 
-
-    methods : {
+    },
+    methods: {
         async login(){
-            if(this.data.email.trim()=='') return this.e('Email is required')
-            if(this.data.password.trim()=='') return this.e('Password is required')
-            if(this.data.password.length < 6) return this.e('Incorrect login details')
-            this.isLogging = true
-            const res = await this.callApi('post', 'app/admin_login', this.data)
-            if(res.status===200){
+        	if(this.data.email.trim()=='') return this.e('Email is required')
+			if(this.data.password.trim()=='') return this.e('Pasword is required')    
+			if(this.data.password.length < 5) return this.e('Please Check Your Password') 
+            this.isLogging = true   
+            const res = await this.callApi('post','app/admin_login', this.data)
+            if (res.status===200) {
                 this.s(res.data.msg)
-                window.location = '/'
             }else{
-                if(res.status===401){
-                    this.i(res.data.msg)
-                }else if(res.status==422){
-                    for(let i in res.data.errors){
-                        this.e(res.data.errors[i][0])
-                    }
-                }
-                else{
+                if (res.status===401) {
+                    this.e(res.data.msg)
+                }else{
                     this.swr()
                 }
             }
-            this.isLogging = false
+            this.isLogging = true
         }
-    }
+    },
 }
 </script>
 
-
-
-
-
-
-<style scoped>
-    ._1adminOverveiw_table_recent {
+<style lang="css" scoped>
+    ._1adminOverveiw_table_recent{
         margin: 0 auto;
         margin-top: 220px;
     }
-    .login_footer{
+    .login-header{
+        padding: 20px 0;
         text-align: center;
     }
-    .login_header{
-        text-align: center;
-        margin-bottom: 25px;
+    .login-footer{
+        margin-top: 20px;
+       text-align: center;
     }
 </style>
